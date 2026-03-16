f2f0a48f036414a2ae9b9d20ba683bbcfe49a157
Stephanie Crich <stephicrich@gmail.com>
2026-03-16 15:29:15 +0000

    Start game buton becomes pause when clicked (might need to change this to 2 separate buttons)

--- src/pages/AIOpponent.jsx
+++ src/pages/AIOpponent.jsx
@@ -1,10 +1,19 @@
 import './AIOpponent.css';
+import { useState } from 'react';
 
 export default function AIOpponent() {
+  const [isGameStarted, setIsGameStarted] = useState(false);
+
+  const toggleGameState = () => {
+    setIsGameStarted((prevState) => !prevState);
+  };
+
   return (
     <div className="aiOpponent">
       <h1 className="aiTitle">AI Opponent</h1>
-      <button className="playButton">Start Game</button>
+      <button className="playButton" onClick={toggleGameState}>
+        {isGameStarted ? "Pause Game" : "Start Game"}
+      </button>
     </div>
   );
 }
